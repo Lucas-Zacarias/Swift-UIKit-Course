6293821d9add3fe549c2882b83f47a0307374a45
lucaszacarias <lucas.zacarias@itrocksweb.com>
2025-02-05 09:51:14 -0300

    initializate first two projects

---
+++ 01-Text-And-Button/01-Text-And-Button/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  01-Text-And-Button
+//
+//  Created by Lucas Zacarias on 05/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 02-Manual-Constraints/02-Manual-Constraints/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  02-Manual-Constraints
+//
+//  Created by Lucas Zacarias on 05/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
