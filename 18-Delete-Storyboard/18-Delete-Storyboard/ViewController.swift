db42242bbd5b6a4761e813b5994058e86f96289b
lucaszacarias <lucas.zacarias@itrocksweb.com>
2025-03-09 10:41:56 -0300

    add an example of a project without storyboard

---
+++ 18-Delete-Storyboard/18-Delete-Storyboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  18-Delete-Storyboard
+//
+//  Created by Lucas Zacarias on 09/03/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
